dc3a9b2d4a5cfebec8dbbd2b7fa1841a33ea50b3
Hristo Petkov <45469690+hristopetkovv@users.noreply.github.com>
2021-02-04 10:31:38 +0200

    add user model

---
+++ InformationSystemServer/InformationSystemServer/Data/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace InformationSystemServer.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public virtual ICollection<Application> Applications { get; set; } = new HashSet<Application>();
+    }
+}

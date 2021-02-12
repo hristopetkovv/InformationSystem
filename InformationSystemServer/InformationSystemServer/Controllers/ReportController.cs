ebc663b746e1fe6f61e451b4ef390a96d167c604
Hristo Petkov <45469690+hristopetkovv@users.noreply.github.com>
2021-02-12 15:07:04 +0200

    add export as excel in back end

--- InformationSystemServer/InformationSystemServer/Controllers/ReportController.cs
+++ InformationSystemServer/InformationSystemServer/Controllers/ReportController.cs
@@ -1,7 +1,9 @@
-﻿using InformationSystemServer.Services.Reference;
+﻿using ClosedXML.Excel;
+using InformationSystemServer.Services.Reference;
 using InformationSystemServer.ViewModels.Application;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace InformationSystemServer.Controllers
@@ -19,6 +21,56 @@ namespace InformationSystemServer.Controllers
         public async Task<IEnumerable<ReportResponseDto>> GetReports([FromQuery] SearchFilterDto filter)
         {
             return await this.reportService.GetReportsAsync(filter);
+        }
+
+        [HttpGet("excel")]
+        public async Task<IActionResult> ExportExcel([FromQuery] SearchFilterDto filter)
+        {
+            var reports = await this.reportService.GetReportsAsync(filter);
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Applications");
+                var currentRow = 1;
+                worksheet.Cell(currentRow, 1).Value = "First Name";
+                worksheet.Cell(currentRow, 2).Value = "Last Name";
+                worksheet.Cell(currentRow, 3).Value = "Municipality";
+                worksheet.Cell(currentRow, 4).Value = "Region";
+                worksheet.Cell(currentRow, 5).Value = "City";
+                worksheet.Cell(currentRow, 6).Value = "Street";
+                worksheet.Cell(currentRow, 7).Value = "Total Course Days";
+                worksheet.Cell(currentRow, 8).Value = "Total Internship Days";
+                worksheet.Cell(currentRow, 9).Value = "Status";
+
+                foreach (var report in reports)
+                {
+                    currentRow++;
+                    worksheet.Cell(currentRow, 1).Value = report.FirstName;
+                    worksheet.Cell(currentRow, 2).Value = report.LastName;
+                    worksheet.Cell(currentRow, 3).Value = report.Municipality;
+                    worksheet.Cell(currentRow, 4).Value = report.Region;
+                    worksheet.Cell(currentRow, 5).Value = report.City;
+                    worksheet.Cell(currentRow, 6).Value = report.Street;
+                    worksheet.Cell(currentRow, 7).Value = report.TotalCourseDays;
+                    worksheet.Cell(currentRow, 8).Value = report.TotalInternshipDays;
+                    worksheet.Cell(currentRow, 9).Value = report.Status.ToString();
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+
+                    const string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    HttpContext.Response.ContentType = contentType;
+                    HttpContext.Response.Headers.Add("Access-Control-Expose-Headers", "Content-Disposition");
+
+                    return File(
+                        content,
+                        contentType,
+                        "reports.xlsx");
+                }
+            }
         }
     }
 }

d93783a2f40a2a2eef4dda61df8ce11b085ffa56
Kristina Marinova <kriska2000@abv.bg>
2021-02-16 09:11:04 +0200

    create own component for messages list

---
+++ InformationSystemClient/src/app/components/messages/message-list/message-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { PostModel } from 'src/app/models/post.model';
+
+@Component({
+  selector: 'message-list',
+  templateUrl: './message-list.component.html',
+  styleUrls: ['./message-list.component.css']
+})
+export class MessageListComponent implements OnInit {
+  @Input() posts: PostModel[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}

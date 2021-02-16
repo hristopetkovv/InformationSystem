785e42853d85de3e24988e235d7a8ec9b70724c2
Kristina Marinova <kriska2000@abv.bg>
2021-02-16 09:07:37 +0200

    remove unnecessary code from home component

--- InformationSystemClient/src/app/components/home/home.component.ts
+++ InformationSystemClient/src/app/components/home/home.component.ts
@@ -13,8 +13,8 @@ export class HomeComponent implements OnInit {
   posts: PostModel[];
   post: PostModel = new PostModel();
   role = JSON.parse(localStorage.getItem('user')).role;
-  show: boolean = false;
-  postStatus = PostStatus;
+
+  // postStatus = PostStatus;
 
   constructor(
     private postService: PostService,
@@ -28,44 +28,5 @@ export class HomeComponent implements OnInit {
   getPosts() {
     this.postService.getPosts()
       .subscribe(data => this.posts = data);
-  }
-
-  addPost() {
-    this.postService.addPost(this.post)
-      .subscribe(data => {
-        this.checkDate(data),
-          this.show = false,
-          this.post = new PostModel()
-      });
-  }
-
-  editPost(currentPost: PostModel) {
-    this.postService.updatePost(currentPost.id, currentPost)
-      .subscribe(data => this.post = data);
-  }
-
-  updatePostStatus(currentPost: PostModel) {
-    this.postService
-      .updatePostStatus(currentPost.id, this.postStatus.Saved)
-      .subscribe(data => {
-        currentPost.status = this.postStatus.Saved
-      });
-  }
-
-
-  checkDate(data) {
-    if (this.isDateBeforeToday(new Date(this.post.startDate))
-      && (this.isDateAfterToday(new Date(this.post.endDate))
-        || this.post.endDate == null)) {
-      this.posts.push(data);
-    }
-  }
-
-  isDateBeforeToday(date) {
-    return new Date(date.toDateString()) <= new Date(new Date().toDateString());
-  }
-
-  isDateAfterToday(date) {
-    return new Date(date.toDateString()) > new Date(new Date().toDateString());
   }
 }
